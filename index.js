df1d62e6347e7d0f458d77b16444ee97fd66e426
LouizEchi <louizechiverri@gmail.com>
2015-10-13 12:36:02 +0800

    Some test updates

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+const _dirname = "test_express";
 var express = require('express');
+var path = require('path'); 
 var app = express();
+var router = express.Router();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('views', path.join(_dirname, 'views'));  
+app.set('view engine', 'jade');
+
+app.use(router);  
+app.use(express.static(path.join(_dirname, 'public')));
+
+router.get('/', function (req, res) {
+	res.render('index',
+	  {
+			title: "Sparta"
+		}
+	);
 });
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
 });
